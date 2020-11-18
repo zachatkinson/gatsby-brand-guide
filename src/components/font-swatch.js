33a5e349e90e221b38e6f6f5ee400f8ca05046f3
zachatkinson <zach.atkinson85@gmail.com>
2020-11-18 16:46:40 -0500

    Build files updated

--- src/components/font-swatch.js
+++ src/components/font-swatch.js
@@ -2,3 +2,17 @@ import React from "react"
 
 
 import "../styles/font-swatch.min.css"
+
+const FontSwatch = (props) => {
+    return (
+
+        <div className={`font-swatch`}>
+            <h2><strong>Futura Regular</strong></h2>
+            <hr />
+            <p className={`typography-sample`}>A B C D E F G H I J K L M N O P Q R S T U V W X Y Z</p>
+            <p className={`typography-sample`}>a b c d e f g h i j k l m n o p q r s t u v w x y z</p>
+            <p className={`typography-sample`}>1 2 3 4 5 6 7 8 9 0 ( ! ? & % $ . , ; : )</p>
+        </div>
+    )
+}
+export default FontSwatch
