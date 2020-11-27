bf031193ad2dcf93855bca20e4b3d64e192c7527
zachatkinson <zach.atkinson85@gmail.com>
2020-11-26 23:47:19 -0500

    Updated logo page to include construction rules

--- src/pages/logo.js
+++ src/pages/logo.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Logo from "../svg/logo.inline.svg"
+import Spacer from "../svg/spacer-demo.inline.svg"
 
 const LogoPage = () => (
     <Layout>
@@ -10,17 +11,26 @@ const LogoPage = () => (
         <div className={`logo-info`}>
             <h2 className={`logo-title`}><strong>Our Logotype</strong></h2>
             <hr/>
+            <p>Our Logo is the key building block of our identity, the primary visual element that identifies us.</p>
             <div className={`sample-wrap`}>
                 <Logo id={`logo-sample`} width={`100%`} height={`auto`}/>
             </div>
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration
-                in some form, by injected humour, or randomised words which don't look even slightly believable. If you
-                are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden
-                in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks
-                as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200
-                Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks
-                reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or
-                non-characteristic words etc.</p>
+
+            <p>The Philly’s logotype consists of a single element, the brand name. The curves present throughout the
+                scripted font elevate a feeling of smoothness and easy riding, an analogy to using Philly’s parts with
+                your skateboard build.</p>
+            <p>The corporate logo is presented through the use of colour as well as shape and form. Bright orange
+                commands attention, further highlighted by the bright blue outline.</p>
+            <h2 className={`logo-title`}><strong>Construction</strong></h2>
+            <hr/>
+            <p>It is important to keep corporate marks clear of any other graphic elements. To regulate this, an
+                exclusion zone has been established around the logotype. This exclusion zone indicates the closest any
+                other graphic element or message can be positioned in relation to the mark of the logotype itself.</p>
+            <div className={`sample-wrap`}>
+                <Spacer className={`spacer-sample`} />
+            </div>
+            <p>The spacing for the Phillys logo can be measured by using the apostrophe like in the example above.</p>
+            <p>The logo must always be presented large enough to clearly read</p>
             <h2 className={`logo-title`}><strong>Do</strong></h2>
             <hr/>
             <h2 className={`logo-title`}><strong>Don't</strong></h2>
